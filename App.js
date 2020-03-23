52538391eee14c4abd6dff354ea29fe1c2896a83
Anant Rungta <anantrungta1999@gmail.com>
2020-03-24 04:11:06 +0530

    Email-Firebase Authentication DONE

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Platform, Image, Text, View } from "react-native";
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import * as firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyBzQAvDa509HP6_MWGGhUkoHWLMqhk2SV0",
+  authDomain: "expo-react-native0.firebaseapp.com",
+  databaseURL: "https://expo-react-native0.firebaseio.com",
+  projectId: "expo-react-native0",
+  storageBucket: "expo-react-native0.appspot.com",
+  messagingSenderId: "278508950004",
+  appId: "1:278508950004:web:6577a5cdaec789b65129a1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// import the different screens
+import Loading from "./components/Loading";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Main from "./components/Main";
+
+// create our app's navigation stack
+const RootStack = createSwitchNavigator(
+  {
+    Loading: Loading,
+    SignUp: SignUp,
+    Login: Login,
+    Main: Main
   },
-});
+  {
+    initialRouteName: "Loading"
+  }
+);
+
+const App = createAppContainer(RootStack);
+export default App;
---
+++ components/Main.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Platform, Button, Image, Text, View } from "react-native";
+import * as firebase from "firebase";
+
+export default class Main extends React.Component {
+  state = { currentUser: null };
+  componentDidMount() {
+    const { currentUser } = firebase.auth();
+    this.setState({ currentUser });
+  }
+  logoutUser = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => this.props.navigation.navigate("Login"));
+  };
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>Hi {currentUser && currentUser.email}!</Text>
+        <Button title="Logout" onPress={this.logoutUser} />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
